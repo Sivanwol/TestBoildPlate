8f37d06e97b77b7541817f4bd2b7f2578f22c9a7
Sivan Wolberg <Sivan@wolberg.pro>
2020-01-21 22:20:01 +0200

    and interview url

--- src/app/SiteTests/sitetests.module.ts
+++ src/app/SiteTests/sitetests.module.ts
@@ -1,12 +1,12 @@
 
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { XsitesComponent } from './components';
-
+import { XsitesComponent } from "./components";
+import {SiteTestsRoutingModule} from "./sitetests-routing.module";
 @NgModule({
   declarations: [XsitesComponent],
   providers: [],
-  imports: [CommonModule]
+  imports: [CommonModule, SiteTestsRoutingModule]
 })
 
 export class SiteTestsModule { }
